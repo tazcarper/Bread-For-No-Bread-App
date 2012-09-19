cd3aefeebefb01d71f9109d45f87522fdfeeaeed
Taz <jtc006@gmail.com>
2012-09-19 12:28:43 -0500

    initial commit for bread

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BreadForNobread::Application.config.secret_token = 'f98eee5d6543b03dcd0c1b533a54b7ab2fe499ef421ececf00012ae6dc10ac3eb4cca653a855f01879d5b620c3444ffc9738fb730383ecbab1e2e87ef14b02f1'
